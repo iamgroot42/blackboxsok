dd9424b326567acb7bbb3911ac5a0282e4d00192
Tingwei-Zhang <tz6rz@virginia.edu>
2022-08-02 11:45:20 -0400

    added testing models

--- code/bbeval/models/pytorch/image.py
+++ code/bbeval/models/pytorch/image.py
@@ -1,10 +1,11 @@
 
 from robustbench.utils import load_model
-from torchvision.models import inception_v3, resnet18, vgg16
+from torchvision.models import inception_v3, resnet18, vgg16, resnet101
 from torchvision import transforms
 
 from bbeval.config import ModelConfig
 from bbeval.models.pytorch.wrapper import PyTorchModelWrapper
+import timm
 
 class Inceptionv3(PyTorchModelWrapper):
     def __init__(self, model_config: ModelConfig):
@@ -60,6 +61,104 @@ class RobustBenchModel(PyTorchModelWrapper):
         self.model = load_model(model_name=self.config.name,
                                 model_dir=self.save_dir,
                                 dataset=self.config.dataset)
+
+class ResNet101(PyTorchModelWrapper):
+    def __init__(self, model_config: ModelConfig):
+        super().__init__(model_config)
+        self.use_pretrained = model_config.use_pretrained
+        self.model = resnet101(pretrained=self.use_pretrained)
+
+    def pre_process_fn(self, x):
+        # imagenet inputs need to be normalized
+        mean = [0.485, 0.456, 0.406]
+        std = [0.229, 0.224, 0.225]
+        transform_norm = transforms.Normalize(mean=mean,
+                                              std=std)
+        return transform_norm(x)
+
+class Inceptionv4(PyTorchModelWrapper):
+    def __init__(self, model_config: ModelConfig):
+        super().__init__(model_config)
+        self.use_pretrained = model_config.use_pretrained
+        self.model = timm.create_model('inception_v4', pretrained=self.use_pretrained)
+
+    def pre_process_fn(self, x):
+        # imagenet inputs need to be normalized
+        mean = [0.485, 0.456, 0.406]
+        std = [0.229, 0.224, 0.225]
+        transform_norm = transforms.Normalize(mean=mean,
+                                              std=std)
+        return transform_norm(x)
+
+class InceptionResNetv2(PyTorchModelWrapper):
+    def __init__(self, model_config: ModelConfig):
+        super().__init__(model_config)
+        self.use_pretrained = model_config.use_pretrained
+        self.model = timm.create_model('inception_resnet_v2', pretrained=self.use_pretrained)
+
+    def pre_process_fn(self, x):
+        # imagenet inputs need to be normalized
+        mean = [0.485, 0.456, 0.406]
+        std = [0.229, 0.224, 0.225]
+        transform_norm = transforms.Normalize(mean=mean,
+                                              std=std)
+        return transform_norm(x)
+
+class EnsAdvInceptionResNetv2(PyTorchModelWrapper):
+    def __init__(self, model_config: ModelConfig):
+        super().__init__(model_config)
+        self.use_pretrained = model_config.use_pretrained
+        self.model = timm.create_model('ens_adv_inception_resnet_v2', pretrained=self.use_pretrained)
+
+    def pre_process_fn(self, x):
+        # imagenet inputs need to be normalized
+        mean = [0.485, 0.456, 0.406]
+        std = [0.229, 0.224, 0.225]
+        transform_norm = transforms.Normalize(mean=mean,
+                                              std=std)
+        return transform_norm(x)
+
+class AdvInceptionv3(PyTorchModelWrapper):
+    def __init__(self, model_config: ModelConfig):
+        super().__init__(model_config)
+        self.use_pretrained = model_config.use_pretrained
+        self.model = timm.create_model('adv_inception_v3', pretrained=self.use_pretrained)
+
+    def pre_process_fn(self, x):
+        # imagenet inputs need to be normalized
+        mean = [0.485, 0.456, 0.406]
+        std = [0.229, 0.224, 0.225]
+        transform_norm = transforms.Normalize(mean=mean,
+                                              std=std)
+        return transform_norm(x)
+
+class ImageNetRobust(PyTorchModelWrapper):
+    def __init__(self, model_config: ModelConfig):
+        super().__init__(model_config)
+        self.use_pretrained = model_config.use_pretrained
+        self.model =  load_model(model_name='Salman2020Do_50_2', dataset='imagenet', threat_model='Linf')
+
+    def pre_process_fn(self, x):
+        # imagenet inputs need to be normalized
+        mean = [0.485, 0.456, 0.406]
+        std = [0.229, 0.224, 0.225]
+        transform_norm = transforms.Normalize(mean=mean,
+                                              std=std)
+        return transform_norm(x)
+
+class Cifar10Robust(PyTorchModelWrapper):
+    def __init__(self, model_config: ModelConfig):
+        super().__init__(model_config)
+        self.use_pretrained = model_config.use_pretrained
+        self.model =  load_model(model_name='Rebuffi2021Fixing_70_16_cutmix_extra', dataset='cifar10', threat_model='Linf')
+
+    def pre_process_fn(self, x):
+        # imagenet inputs need to be normalized
+        mean = [0.485, 0.456, 0.406]
+        std = [0.229, 0.224, 0.225]
+        transform_norm = transforms.Normalize(mean=mean,
+                                              std=std)
+        return transform_norm(x)
 
 # _MODEL_MAPPING = {
 #     "inceptionv3": Inceptionv3,
--- code/bbeval/models/utils.py
+++ code/bbeval/models/utils.py
@@ -4,7 +4,14 @@ from bbeval.config import ModelConfig
 MODEL_WRAPPER_MAPPING = {
     "inceptionv3": image.Inceptionv3,    
     "resnet18": image.ResNet18,
-    "vgg16": image.VGG16
+    "vgg16": image.VGG16,
+    "resnet101": image.ResNet101,
+    "inceptionv4": image.Inceptionv4,
+    "inceptionresnetv2": image.InceptionResNetv2,
+    "ensinceptionresnetv2": image.EnsAdvInceptionResNetv2,
+    "advinceptionv3": image.AdvInceptionv3,
+    "imagenetrobust": image.ImageNetRobust,
+    "cifar10robust": image.Cifar10Robust,
 }
 
 
