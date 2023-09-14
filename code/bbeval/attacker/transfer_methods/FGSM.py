dded9d9f543b01ef133a9100721280826daeb8cf
Tingwei Zhang <73029719+Tingwei-Zhang@users.noreply.github.com>
2023-09-14 15:37:13 -0400

    Update FGSM.py

--- code/bbeval/attacker/transfer_methods/FGSM.py
+++ code/bbeval/attacker/transfer_methods/FGSM.py
@@ -12,6 +12,7 @@ from bbeval.attacker.transfer_methods._manipulate_gradient import torch_staircas
 from bbeval.attacker.transfer_methods._manipulate_input import ensemble_input_diversity, input_diversity, clip_by_tensor
 
 import torchvision.models as models  # TODO: remove after test
+import time
 
 np.set_printoptions(precision=5, suppress=True)
 
@@ -26,9 +27,10 @@ class FGSM(Attacker):
         self.queries = 1
         self.criterion = get_loss_fn("ce")
         self.norm = None
+        self.attacker_config=AttackerConfig
 
 
-    def attack(self, x_orig, x_adv=None, y_label=None, x_target=None, y_target=None):
+    def attack(self, x_orig, x_adv=None, y_label=None, x_target=None, y_target=None, experiment_file_name=None):
         """
             Attack the original image using combination of transfer methods and return adversarial example
             (x, y_label): original image
@@ -37,7 +39,7 @@ class FGSM(Attacker):
         targeted = self.targeted
         image_resizes = self.params.image_resizes
         image_width = self.params.image_width
-        n_iters = 1
+        n_iters = self.params.n_iters
         interpol_dim = self.params.interpol_dim  # Not sure why this thing is different
 
         if not isinstance(self.aux_models, dict):
@@ -61,6 +63,7 @@ class FGSM(Attacker):
         # quite specific piece of code to staircase attack
         x_min = clip_by_tensor(x_orig - eps, x_min_val, x_max_val)
         x_max = clip_by_tensor(x_orig + eps, x_min_val, x_max_val)
+        sum_time=0
 
         for model_name in self.aux_models:
             model = self.aux_models[model_name]
@@ -70,7 +73,9 @@ class FGSM(Attacker):
             # corr_classified = ch.argmax(logits_clean, dim=1) == y_label
             # print('Clean accuracy of candidate samples: {:.2%}'.format(ch.mean(1. * corr_classified).item()))
 
-        for i in range(n_iters):
+        for i in range(1):
+            start_time = time.time()
+
             if i == 0:
                 adv = clip_by_tensor(adv, x_min, x_max)
                 adv = V(adv, requires_grad=True)
@@ -92,6 +97,29 @@ class FGSM(Attacker):
                 adv = adv + alpha*gradient_sign
             adv = clip_by_tensor(adv, x_min, x_max)
             adv = V(adv, requires_grad=True)
+            end_time = time.time()
+            sum_time=end_time-start_time+sum_time
+            # outputs the transferability
+            self.model.set_eval()  # Make sure model is in eval model
+            self.model.zero_grad()  # Make sure no leftover gradients
+            target_model_output = self.model.forward(adv)
+            target_model_prediction = ch.max(target_model_output, 1).indices
+            batch_size = len(y_target)
+            if targeted:
+                num_transfered = ch.count_nonzero(target_model_prediction == y_target)
+            else:
+                num_transfered = ch.count_nonzero(target_model_prediction != y_target)
+            # print(num_transfered)
+            transferability = float(num_transfered / batch_size) * 100
+
+            with open(experiment_file_name, 'a') as f:
+                f.write("iteration: %s" % (str(i)))
+                f.write('\n')
+                f.write("time: %s" % (str(sum_time)))
+                f.write('\n')
+                f.write("ASR: %s" % (str(transferability)))
+                f.write('\n')
+
 
         stop_queries = 1
         # print(abs(adv - x_orig))
